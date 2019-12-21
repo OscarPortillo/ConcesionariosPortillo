f4aca6282dd846c67044e8911204a93724501a2b
oscar_9414 <oscar9414ps@gmail.com>
2019-12-21 21:37:07 +0100

    403 arreglado

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -81,7 +81,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                @if(count($errorCliente)!=0)
+                @if($errorCliente)!=null)
                 {{$errorCliente}}
                 @endif
             </div>
