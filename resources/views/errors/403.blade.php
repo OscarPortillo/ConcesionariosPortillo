1d903ebc30c550c1c27b121bfead240f56921520
oscar_9414 <oscar9414ps@gmail.com>
2019-12-21 21:44:17 +0100

    403 arreglado

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -81,7 +81,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                @if($errorCliente != null)
+                @if(Session::has('errorCliente'))
                     {{$errorCliente}}
                 @else 
                     no tienes acceso!!!
