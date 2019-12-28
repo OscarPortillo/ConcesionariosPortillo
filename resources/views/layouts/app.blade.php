b981065f6dddab165d911231c40e0c24eb1fc6b0
oscar_9414 <oscar9414ps@gmail.com>
2019-12-28 17:23:51 +0100

    La página definitiva

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Concesionarios Portillo') }}</title>
+    <title>Concesionarios Portillo</title>
+    <!--<title>{{ config('app.name', 'Concesionarios Portillo') }}</title>-->
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
